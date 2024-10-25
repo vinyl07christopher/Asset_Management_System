857cfca32adda659b7160cac8bd6878fb9112743
Vinyl Christopher <86916576+vinyl07christopher@users.noreply.github.com>
2024-10-25 17:31:07 +0530

    Delete routes/api directory

--- routes/api/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { create, login, logout } = require("../../controllers/auth.controller");
-const router = express.Router();
-
-// router.post("/register", create);
-router.get("/login", login);
-router.post("/logout", logout);
-
-module.exports = router;
